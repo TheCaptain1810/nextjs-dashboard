937902680d5b024187a3558c792e91c55464de7f
Pranav Pise <itspranavpise@gmail.com>
2025-01-13 09:40:47 +0530

    implemented error handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
  
 export default async function Page(props: {
     params: Promise<{id: string}>
@@ -10,7 +11,11 @@ export default async function Page(props: {
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers(),
-    ])
+    ]);
+
+    if (!invoice) {
+        return notFound();
+    }
 
   return (
     <main>
